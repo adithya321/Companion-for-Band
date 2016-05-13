5f8f941472c529bcc72510e474d8e2aeac816d26
Adithya J <adithya321@hotmail.com>
2016-05-13 15:02:02 +0530

    UnitTest Test

---
+++ app/src/test/java/com/pimp/companionforband/UnitTest.java
@@ -0,0 +1,13 @@
+package com.pimp.companionforband;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class UnitTest {
+
+    @Test
+    public void unitTest1() {
+        assertTrue(1 + 1 == 2);
+    }
+}
